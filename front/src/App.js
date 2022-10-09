2e386fa87473d45bae1afc73e584f56fbf10bd7e
Jacob Willemsz <willemszjacob@gmail.com>
2022-10-09 12:54:43 -0700

    Task 3 for web dev team is done. I created another text input for a username and image description and a file input for the image.

--- front/src/App.js
+++ front/src/App.js
@@ -5,48 +5,49 @@ import React, { useEffect, useRef, useState } from 'react';
 
 function App() {
 
-  const [num, setNum] = useState(0)
-
-  const whenButtonClick = () => {
-    setNum(num + 1)
-  }
-
   const whenSubmit = () => {
     alert("Thanks for submitting a response")
   }
 
-  useEffect(() => {
-    document.title = `You clicked ${num} times`
-  })
-
-
-
   return (
 
     <div>
-      <div>Number: {num}</div>
-      <button onClick={whenButtonClick}>button</button>
-      <div>You clicked {num} times</div>
+      <p>
+        <form onSubmit={whenSubmit}>
+          <div>
+            <label>
+              Username:
+              <input type="text" name="name" />
+            </label>
+          </div>
+          <div>
+            <label>
+              Image Description:
+              <input type="text" name="image description" />
+            </label>
+            <div><input type="submit" value="Submit" /></div>
+          </div>
+        </form>
+      </p>
 
-      <form onSubmit={whenSubmit}>
-        <div>
-          <label>
-            Username:
-            <input type="text" name="name" />
-          </label>
+      <p>
+        <form onSubmit={whenSubmit}>
+          <div>
+            <label>
+              Username:
+              <input type="text" name="name" />
+            </label>
+          </div>
 
-        </div>
-        <div>
-          <label>
-            Reported Problem:
-            <input type="text" text="problem" />
-          </label>
-          <div><input type="submit" value="Submit" /></div>
-        </div>
-
-      </form>
-
-
+          <div>
+            <label>
+              Reported Problem:
+              <input type="text" text="problem" />
+            </label>
+            <div><input type="submit" value="Submit" /></div>
+          </div>
+        </form>
+      </p>
     </div>
 
   );
